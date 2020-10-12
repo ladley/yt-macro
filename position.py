6c1393cb26d645635c2390fbccfa2d09b36bc2b5
ladley <42934022+ladley@users.noreply.github.com>
2020-10-13 01:23:33 +0900

    ahk 버전

--- position.py
+++
@@ -1,4 +0,0 @@
-#demo.py
-import pyautogui
-
-print(pyautogui.position())
--- skipfind.py
+++
@@ -1,7 +0,0 @@
-import pyautogui as pag
-
-pag.click(x=366, y=174)
-pos = pag.locateCenterOnScreen('images/skipBtn.png')
-pag.moveTo(pos)
-
-print(pos)
